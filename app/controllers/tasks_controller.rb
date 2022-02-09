fbd146d5a8b9266186c7de86d4dc0ab30d2f4917
kazuki.hori <git123.hoppy@gmail.com>
2022-02-09 03:25:31 +0000

    finish tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,8 +53,9 @@ class TasksController < ApplicationController
     private
     
     
+    
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content,:status)
     end
     
     
